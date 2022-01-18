ed5176ac6351302a65f6ec72a26337d913ee79ee
Vladimir Petrov <vl.pva@yandex.ru>
2022-01-18 23:10:34 +0300

    add two panel

--- app/src/main/java/ru/gb/notesmanager/ui/MainActivity.java
+++ app/src/main/java/ru/gb/notesmanager/ui/MainActivity.java
@@ -6,10 +6,14 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import ru.gb.notesmanager.App;
 import ru.gb.notesmanager.domain.NoteRepository;
@@ -20,9 +24,10 @@ import ru.gb.notesmanager.ui.list.NoteAdapter;
 import ru.gb.notesmanager.ui.list.NotesListFragment;
 
 public class MainActivity extends AppCompatActivity
-        implements NotesListFragment.Controller, NoteDetailsFragment.Controller{
+        implements NotesListFragment.Controller, NoteDetailsFragment.Controller {
     private static final String TAG = "@@@";
     private static final String TAG_NOTES_LIST_FRAGMENT = "TAG_NOTES_LIST_FRAGMENT";
+    private static final String TAG_NOTE_DETAILS_FRAGMENT = "TAG_NOTE_DETAILS_FRAGMENT";
     private boolean addNote;
 
 
@@ -31,26 +36,70 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addNote = false;
-        if (savedInstanceState == null) {
-            Fragment notesListFragment = new NotesListFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.activity_main__list_fragment_container, notesListFragment, TAG_NOTES_LIST_FRAGMENT)
-                    .commit();
+        if (isTwoPaneMode()) {
+            Fragment noteFragment = getSupportFragmentManager().findFragmentById(R.id.activity_main__list_fragment_container);
+            if (noteFragment instanceof NoteDetailsFragment) {
+                moveFragment(noteFragment);
+                //showListFragment();
+            }
+            // showListFragment();
         }
-
+        showListFragment();
 
 
     }
 
-    @Override
-    public void addNote() {
-        NotesEntity noteEntity = new NotesEntity(String.valueOf(App.get(this).getNoteRepository().getSize()),"","");
-        addNote = true;
-        Fragment noteDetailsFragment = NoteDetailsFragment.newInstance(noteEntity);
+    private void moveFragment(Fragment oldFragment) {
+        final Bundle args = oldFragment.getArguments();
+        final Bundle savedState = new Bundle();
+        oldFragment.onSaveInstanceState(savedState);
+
+        Fragment newFragment = null;
+        try {
+            newFragment = oldFragment.getClass().newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        newFragment.setArguments(args);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.activity_main__list_fragment_container, noteDetailsFragment)
+                .replace(R.id.activity_main__details_fragment_container, newFragment, TAG_NOTE_DETAILS_FRAGMENT)
+                .addToBackStack(null)
+                .commit();
+        //newFragment.onAttach((Context) this);
+        //FrameLayout container = findViewById(R.id.activity_main__details_fragment_container);
+        //final View fragmentView = newFragment.onCreateView(LayoutInflater.from(this), container, savedState);
+        //container.addView(fragmentView);
+        //newFragment.onViewCreated(fragmentView, savedState);
+    }
+
+    private boolean isTwoPaneMode() {
+        FrameLayout detailsFragmentContainer = findViewById(R.id.activity_main__details_fragment_container);
+        return detailsFragmentContainer != null;
+    }
+
+    private void showListFragment() {
+        Fragment notesListFragment = new NotesListFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.activity_main__list_fragment_container, notesListFragment, TAG_NOTES_LIST_FRAGMENT)
+                .commit();
+    }
+
+    @Override
+    public void addNote() {
+        NotesEntity noteEntity = new NotesEntity(String.valueOf(App.get(this).getNoteRepository().getSize()), "", "");
+        addNote = true;
+        Fragment noteDetailsFragment = NoteDetailsFragment.newInstance(noteEntity);
+        int contanerId = R.id.activity_main__list_fragment_container;
+        if (isTwoPaneMode()) {
+            contanerId = R.id.activity_main__details_fragment_container;
+        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(contanerId, noteDetailsFragment)
                 .addToBackStack(null)
                 .commit();
     }
@@ -58,9 +107,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void showNoteDetails(NotesEntity noteEntity) {
         Fragment noteDetailsFragment = NoteDetailsFragment.newInstance(noteEntity);
+        int contanerId = R.id.activity_main__list_fragment_container;
+        if (isTwoPaneMode()) {
+            contanerId = R.id.activity_main__details_fragment_container;
+        }
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.activity_main__list_fragment_container, noteDetailsFragment)
+                .replace(contanerId, noteDetailsFragment)
                 .addToBackStack(null)
                 .commit();
     }
@@ -70,7 +123,7 @@ public class MainActivity extends AppCompatActivity
         getSupportFragmentManager().popBackStack();
         App.get(this).getNoteRepository().deleteNote(noteEntity);
         NotesListFragment notesListFragment = (NotesListFragment) getSupportFragmentManager().findFragmentByTag(TAG_NOTES_LIST_FRAGMENT);
-        if(notesListFragment == null) {
+        if (notesListFragment == null) {
             throw new IllegalArgumentException("NotesListFragment not on screen");
         }
         notesListFragment.onDeleteEmployee(noteEntity);
@@ -79,10 +132,13 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onOkButtonDetails(NotesEntity noteEntity) {
         getSupportFragmentManager().popBackStack();
-        if ( addNote ) { App.get(this).getNoteRepository().addNote(noteEntity); }
-        else { App.get(this).getNoteRepository().updateNote(noteEntity); }
+        if (addNote) {
+            App.get(this).getNoteRepository().addNote(noteEntity);
+        } else {
+            App.get(this).getNoteRepository().updateNote(noteEntity);
+        }
         NotesListFragment notesListFragment = (NotesListFragment) getSupportFragmentManager().findFragmentByTag(TAG_NOTES_LIST_FRAGMENT);
-        if(notesListFragment == null) {
+        if (notesListFragment == null) {
             throw new IllegalArgumentException("NotesListFragment not on screen");
         }
         notesListFragment.updateNoteList();
@@ -91,5 +147,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onCancelButtonDetails() {
         getSupportFragmentManager().popBackStack();
+
     }
 }
